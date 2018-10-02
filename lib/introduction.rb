929b96e5fdb5f152f16d24454a1bedfa4d802256
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-10-02 14:10:35 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,3 +2,6 @@ def introduction(name)
   puts "Hi, My name is #{name}."
 end 
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
